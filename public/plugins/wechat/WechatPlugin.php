ff51327178bd094f0c4487881120eabc13d296cd
yibin94 <964845756@qq.com>
2018-03-10 16:00:17 +0800

    测试微信公众号回调信息接收

--- public/plugins/wechat/WechatPlugin.php
+++ public/plugins/wechat/WechatPlugin.php
@@ -103,6 +103,49 @@ INSERT;
                     		);
                 $weObj = new TpWechat($options);
                 $weObj->valid();
+				
+				
+   $menu = array (
+      	    'button' => array (
+      	      0 => array (
+      	        'name' => '扫码',
+      	        'sub_button' => array (
+     	            0 => array (
+      	              'type' => 'scancode_waitmsg',
+      	              'name' => '扫码带提示',
+      	              'key' => 'rselfmenu_0_0',
+      	            ),
+      	            1 => array (
+      	              'type' => 'scancode_push',
+     	              'name' => '扫码推事件',
+      	              'key' => 'rselfmenu_0_1',
+      	            ),
+      	        ),
+      	      ),
+      	      1 => array (
+     	        'name' => '发图',
+      	        'sub_button' => array (
+      	            0 => array (
+      	              'type' => 'pic_sysphoto',
+      	              'name' => '系统拍照发图',
+      	              'key' => 'rselfmenu_1_0',
+      	            ),
+      	            1 => array (
+     	              'type' => 'pic_photo_or_album',
+      	              'name' => '拍照或者相册发图',
+      	              'key' => 'rselfmenu_1_1',
+      	            )
+      	        ),
+      	      ),
+      	      2 => array (
+      	        'type' => 'location_select',
+      	        'name' => '发送位置',
+      	        'key' => 'rselfmenu_2_0'
+     	      ),
+      	    ),
+      	);				
+				$weObj->createMenu($menu);
+				
                 //用户openid:
                 $openid = $weObj->getRev()->getRevFrom();
                 $type = $weObj->getRev()->getRevType();
