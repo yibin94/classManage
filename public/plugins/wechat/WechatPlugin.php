c97fa78821b5010950904224807ee2b66394628c
yibin94 <964845756@qq.com>
2018-03-10 21:04:48 +0800

    测试创建菜单

--- public/plugins/wechat/WechatPlugin.php
+++ public/plugins/wechat/WechatPlugin.php
@@ -148,13 +148,13 @@ INSERT;
 				$weObj->text("您的")->reply();
                     exit;*/
 					
-$newmenu =  [
+$newmenu =  array(
     		"button"=>
-    			[
-    				['type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'],
-    				['type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com']
-    			]
-   		    ];
+    			array(
+    				array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
+    				array('type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com'),
+    				)
+   		);
     $result = $weObj->createMenu($newmenu);					
 					
 					
