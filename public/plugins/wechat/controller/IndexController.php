5920988f28d35074bb3891a3932c3a2443232051
yibin94 <964845756@qq.com>
2018-03-13 15:58:20 +0800

    测试模板

--- public/plugins/wechat/controller/IndexController.php
+++ public/plugins/wechat/controller/IndexController.php
@@ -185,7 +185,7 @@ class IndexController extends PluginBaseController{
             'username.require' => '请输入手机号!',
         ]);
 
-        $data = $this->request->param();console.log($data);die;
+        $data = $this->request->param();echo '<script>alert('.$data['username'].');</script>';echo '<script>alert('.$data['mobile'].');</script>';die;
         if (!$validate->check($data)) {
             $this->error($validate->getError());
         }
