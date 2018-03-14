ebbec0a39522bbbcca8ee08e9ee6c00bbeb91133
yibin94 <964845756@qq.com>
2018-03-14 10:18:29 +0800

    测试获取session

--- public/plugins/wechat/controller/IndexController.php
+++ public/plugins/wechat/controller/IndexController.php
@@ -195,7 +195,7 @@ class IndexController extends PluginBaseController{
         if (preg_match('/(^(13\d|15[^4\D]|17[013678]|18\d)\d{8})$/', $data['username'])) {
             $accountType = 'mobile';
         } else {
-            $this->error("请输入正确的手机格式!");
+            $this->error("请输入正确的手机格式!{:session('a')}");
         }
 
         //TODO 限制 每个ip 的发送次数
