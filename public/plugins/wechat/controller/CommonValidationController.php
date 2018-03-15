96f6d751ffe7bb5fdadd3c110443937cc6db3d6b
yibin94 <964845756@qq.com>
2018-03-15 17:03:31 +0800

    测试session

--- public/plugins/wechat/controller/CommonValidationController.php
+++ public/plugins/wechat/controller/CommonValidationController.php
@@ -15,13 +15,16 @@ use cmf\controller\PluginBaseController;
 use plugins\wechat\model\PluginWechatModel;
 
 class CommonValidationController extends PluginBaseController{
-    /* 公共验证控制器初始化验证是否已经登录 */
+    /* 公共验证控制器初始化验证是否已经登录. */
 	protected function _initialize()
     {
 		parent::_initialize();
-		//获取sesion中的用户id，可判断是否登录
+		$openid = session('openid','','thinkcmf');
+		$this->assign('openid',$openid);
+		return $this->fetch("/index/index");
+		//获取sesion中的用户id，可判断是否登录.
         $userId = session('PLUGIN_WECHAT_USER_ID');
-        if (!$userId) {
+        if (!$userId) {//无用户id则跳回登录界面.
             return $this->redirect(cmf_plugin_url("Wechat://Index/login"));
         }
     }
