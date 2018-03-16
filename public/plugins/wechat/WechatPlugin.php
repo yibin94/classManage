dd8b4f0c31f25a0988f8703e4ac906c860c02320
yibin94 <964845756@qq.com>
2018-03-16 22:54:03 +0800

    测试

--- public/plugins/wechat/WechatPlugin.php
+++ public/plugins/wechat/WechatPlugin.php
@@ -195,7 +195,7 @@ SQL;
            		    /* 检测事件类型 */
            		    switch ($rev_event['event']){
            		        case TpWechat::EVENT_MENU_CLICK:
-						$callback = 'http://www.shibin.tech/classManage/public/plugin/wechat/Index/index';
+						$callback = 'http://www.shibin.tech/classManage/public/plugin/wechat/Index/index.html';
 						$code = $weObj->getOauthRedirect($callback,'','snsapi_userinfo');
 						//$weObj->getOauthAccessToken();
 						$weObj->text($code)->reply();
