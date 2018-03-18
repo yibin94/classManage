146640b44833426053c82eed96e28665b6bf293d
yibin94 <964845756@qq.com>
2018-03-18 20:25:37 +0800

    修改图像接口文件路径

--- public/plugins/wechat/api/Wechat/jssdk.php
+++ public/plugins/wechat/api/Wechat/jssdk.php
@@ -67,7 +67,7 @@ class JSSDK {
 
   private function getAccessToken() {
     // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
-    $data = json_decode($this->get_php_file("/classManage/public/plugins/wechat/api/Wechat/access_token.php"));
+    $data = json_decode($this->get_php_file("/webdata/classManage/public/plugins/wechat/api/Wechat/access_token.php"));
     if ($data->expire_time < time()) {
       // 如果是企业号用以下URL获取access_token
       // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
