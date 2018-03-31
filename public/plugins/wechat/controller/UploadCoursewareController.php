413617114b580ae17cc4c24b7557fc76b1ffe002
yibin94 <964845756@qq.com>
2018-03-31 19:29:27 +0800

    test ajax 图像接口上传图片

--- public/plugins/wechat/controller/UploadCoursewareController.php
+++ public/plugins/wechat/controller/UploadCoursewareController.php
@@ -24,15 +24,31 @@ class UploadCoursewareController extends PluginBaseController{
 	    //根据微信JS接口上传了图片,会返回上面写的images.serverId（即media_id），填在下面即可  
 		 $getDataUrl = "https://api.weixin.qq.com/cgi-bin/media/get?access_token=".$res['refresh_token']."&media_id=$media_id";  
 		 //获取微信"获取临时素材"接口返回来的内容（即刚上传的图片数据）  
-		 $data = file_get_contents($getDataUrl);  
+		 //$data = file_get_contents($getDataUrl);  
 		 //__DIR__指向当前执行的PHP脚本所在的目录  
-		 echo __DIR__;
+		 //echo __DIR__;
+		 /*
 		 //以读写方式打开一个文件，若没有，则自动创建  
 		 $resource = fopen(__DIR__."/".time().".jpg" , 'w+');  
 		 //将图片内容写入上述新建的文件  
 		 fwrite($resource, $data);  
 		 //关闭资源  
 		 fclose($resource);
+		*/
+		
+		$ch = curl_init($getDataUrl);
+        $ranfilename=time().rand().".jpg";
+		$path = __DIR__;
+        $filename=$path.'/'.date('Y_m_d').'/'.$ranfilename;
+        //$tarfilename=$tardir."/".$ranfilename;
+        $fp = fopen($filename, "w");
+         
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+ 
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
 		
 		return ;
 	}
