14d0f163f3a99c1f642d5cbc18cd556e984c61db
yibin94 <964845756@qq.com>
2018-04-25 00:46:48 +0800

    test upload file

--- public/plugins/wechat/controller/UploadCoursewareController.php
+++ public/plugins/wechat/controller/UploadCoursewareController.php
@@ -8,9 +8,9 @@ use think\Validate;
 use think\Db;
 
 class UploadCoursewareController extends PluginBaseController{
-    function index(){
+    function index(){var_dump(request());die;
 		// 获取表单上传文件 例如上传了001.jpg    
-		 $file = request()->file('file');    var_dump(request());die;
+		 $file = request()->file('file');    
 		 // 移动到框架应用根目录/public/upload/ 目录下    
 		 $info = $file->move(ROOT_PATH . 'public/upload');
 		 if($info){
