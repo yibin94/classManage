e1c2a7d7d4cafa26ca843d64139d18baca4b4fd8
yibin94 <964845756@qq.com>
2018-04-29 23:11:04 +0800

    test auth login

--- public/plugins/wechat/controller/CommonValidationController.php
+++ public/plugins/wechat/controller/CommonValidationController.php
@@ -8,7 +8,7 @@ use think\Request;
 
 class CommonValidationController extends PluginBaseController{
 
-	const CONFIG = new CommonValidationController()->getPlugin()->getConfig();
+	const CONFIG = self::getPlugin()->getConfig();
 	protected static function getWeObj(){
 		if(!isset($weObj)){
 			$options = array(
