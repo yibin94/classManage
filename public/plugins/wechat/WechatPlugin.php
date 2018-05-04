f67fe6f813061a8126d51a53556cbbf2b5e886eb
yibin94 <964845756@qq.com>
2018-05-05 00:35:10 +0800

    测试签到错误

--- public/plugins/wechat/WechatPlugin.php
+++ public/plugins/wechat/WechatPlugin.php
@@ -209,7 +209,8 @@ SQL;
            		    switch ($rev_event['event']){
            		        case TpWechat::EVENT_MENU_CLICK:
            		            //TODO:CLICK事件
-                          $weObj->text($weObj->EventKey)->reply();die;
+                          $rec = $weObj->getRevData();
+                          $weObj->text($rec['EventKey'])->reply();die;
                           switch ($weObj->EventKey) {//获取设置的key值。
                             case 'MENU_KEY_SIGNIN'://签到操作
                               $record = Db::name('PluginWechatSignin')->where('openid',$openid)->order('id DESC')->limit(1)->find();
