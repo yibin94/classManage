7817803769e5110eb91a4b35766ad50452eee98e
yibin94 <964845756@qq.com>
2018-05-05 00:39:34 +0800

    签到错误 again

--- public/plugins/wechat/WechatPlugin.php
+++ public/plugins/wechat/WechatPlugin.php
@@ -212,7 +212,7 @@ SQL;
                           $receiveData = $weObj->getRevData();
                           switch ($receiveData['EventKey']) {//获取设置的key值。
                             case 'MENU_KEY_SIGNIN'://签到操作
-                              $record = Db::name('PluginWechatSignin')->where('openid',$openid)->order('id DESC')->limit(1)->find();
+                              $record = Db::name('PluginWechatSignin')->where('openid',$openid)->order('id DESC')->limit(1)->select();
                               $studentId = '';$weObj->text($revCont)->reply(json_encode($record));die;
                               if(!empty($record)){
                                 $studentId = $record['studentId'];
